f7a07a3fd10919cd69709bb6b6c49bd0b65a1981
praveenchary17 <52570158+praveenchary17@users.noreply.github.com>
2020-02-20 13:19:01 +1100

    Updated my index.php file
    Updated my index.php file

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
-echo "THis is my integration from git lab to jenkins to AWS EC2 using code deploy";
+echo "THis is my integration from git lab to jenkins to code deploy to AWS EC2 using code deploy";
 
 ?>
